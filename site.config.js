914c5f2530ce863c209249d31249581e91146a94
Andras Gulacsi <guland@protonmail.com>
2019-09-28 22:34:21 +0200

    change favicon, 404 page, cover img etc

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ module.exports = {
   site: {
     url: '/',
     title: `KizombaSzeged–Kizomba és semba táncoktatás Szegeden`,
-    defaultImage: '/assets/images/static.jpg',
+    defaultImage: '/assets/images/kizombaszeged.jpg',
     author: 'Gulácsi András',
     quote: 'Kizomba Szeged–Érdekel az afrikai társastáncok világa? Szeretnél úgy táncolni, mint egy afrikai?',
     description: 'Kizomba Szeged–Kizomba és semba táncoktatás Szegeden',
@@ -11,6 +11,6 @@ module.exports = {
     gulacsiAndras: 'https://www.facebook.com/andras.gulacsi.3',
     szlavatiAndrea: 'https://www.facebook.com/andrea.szlavati',
     google_analytics: 'YOUR-ID-COMES-HERE',
-    eventLink: 'https://www.facebook.com'
+    workshopLink: 'https://www.facebook.com'
   }
 }
