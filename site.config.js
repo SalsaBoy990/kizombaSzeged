f540a684378d71fed2149d2facba537005aa17e0
Andras Gulacsi <guland@protonmail.com>
2019-09-29 14:06:33 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 // Add your global site properties here
 module.exports = {
   site: {
-    url: '/',
+    url: 'https://kizombaszeged.hu/',
     title: `KizombaSzeged–Kizomba és semba táncoktatás Szegeden`,
     defaultImage: '/assets/images/kizombaszeged.jpg',
     author: 'Gulácsi András',
